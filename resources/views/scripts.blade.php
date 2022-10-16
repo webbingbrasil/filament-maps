1ae762a6a1d5e4728f6e39f4a813d41c6c0fb4aa
Danilo Andrade <mandrade.danilo@gmail.com>
2022-10-16 15:55:18 -0300

    fix: adjust draw bar style

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -53,6 +53,7 @@
         }
         this.buttonsDomNode = this._makeButton(this._button);
         this._container.classList.remove('leaflet-bar');
+        this._container.classList.add('rounded-lg', 'border', 'border-gray-300', 'overflow-hidden');
         this._container.appendChild(this.buttonsDomNode);
 
         return this._container;
@@ -76,9 +77,9 @@
         const newButton = L.DomUtil.create(
             'button',
             'filament-button filament-button-size-md inline-flex items-center justify-center py-1 gap-1 font-medium ' +
-            'rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 ' +
+            'border-b border-gray-300 transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 ' +
             'focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-2 text-sm text-gray-800 bg-white ' +
-            'border-gray-300 hover:bg-gray-50 focus:ring-primary-600 focus:text-primary-600 focus:bg-primary-50 ' +
+            'hover:bg-gray-50 focus:ring-primary-600 focus:text-primary-600 focus:bg-primary-50 ' +
             'focus:border-primary-600 dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-gray-600 ' +
             'dark:hover:border-gray-500 dark:text-gray-200 dark:focus:text-primary-400 dark:focus:border-primary-400 ' +
             'dark:focus:bg-gray-800 filament-page-icon-button-action',

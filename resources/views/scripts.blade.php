6f909871a921bda015072fa2f2d605f7272a2e67
Danilo Andrade <mandrade.danilo@gmail.com>
2022-10-19 11:31:33 -0300

    fix: use minified version of leaflet assets and fix a bug on popup zooming

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,8 +1,20 @@
-<script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
-        integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg="
-        crossorigin=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.2/leaflet.min.js"
+        integrity="sha512-QPePsP1y4BuymxYZEuQdJ4dsUzZR9MmyrMOLK//zHK4ZoDfMA7zQOJefboRSIm6QKCfzC7djwDOO/+0f72w7Zg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/gh/jerroydmoore/leaflet-button@master/L.Control.Button.js"></script>
 <script>
+    window.filamentMaps = window.filamentMaps || [];
+
+
+    L.Popup.prototype._animateZoom = function (e) {
+        if (!this._map) {
+            return
+        }
+        var pos = this._map._latLngToNewLayerPoint(this._latlng, e.zoom, e.center),
+            anchor = this._getAnchor();
+        L.DomUtil.setPosition(this._container, pos.add(anchor));
+    }
+
     L.Tooltip.prototype._updatePosition = function (e) {
         if (!this._map) {
             return
@@ -11,12 +23,12 @@
         var pos = this._map.latLngToLayerPoint(this._latlng);
         this._setPosition(pos);
     };
+
     L.Tooltip.prototype._animateZoom = function (e) {
         if (!this._map) {
             return
         }
         var pos = this._map._latLngToNewLayerPoint(this._latlng, e.zoom, e.center);
         this._setPosition(pos);
-    }
-    window.filamentMaps = window.filamentMaps || [];
+    };
 </script>
--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -1,31 +1,12 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.css">
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
-      integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
-      crossorigin=""/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.2/leaflet.min.css"
+      integrity="sha512-ekzf1ud/EngtmWo8xbIkhtBQsSgkOhTyyD6AA6b/J4/YXTInuNgeKVQDCH/pysNYnrGgsZazWSuWsNtFKzHbmg=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
 <style>
     .filament-map-button button,
     .filament-map-button a,
     .filament-map-button button:hover,
     .filament-map-button a:hover {
         display: flex;
-    }
-    .filament-maps-popover-arrow.filament-maps-popover-arrow-right {
-        top: 50%;
-        right: -16px;
-        transform: translate(-50%, -50%) rotate(225deg);
-    }
-    .filament-maps-popover-arrow.filament-maps-popover-arrow-left {
-        top: 50%;
-        left: 0;
-        transform: translate(-50%, -50%) rotate(45deg);
-    }
-    .filament-maps-popover-arrow {
-        height: 16px;
-        width: 16px;
-        background-color: #fff;
-        position: absolute;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-        border-left: 1px solid rgba(0, 0, 0, 0.3);
-        transform: translate(-50%, -50%) rotate(45deg);
     }
 </style>

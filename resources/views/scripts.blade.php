e55646de7641a5b5f9d25b6d5dc9679bba0f7414
Danilo Andrade <mandrade.danilo@gmail.com>
2022-11-04 21:32:43 -0300

    fix: prevent error on marker animation

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,12 +1,10 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.2/leaflet.min.js"
-        integrity="sha512-QPePsP1y4BuymxYZEuQdJ4dsUzZR9MmyrMOLK//zHK4ZoDfMA7zQOJefboRSIm6QKCfzC7djwDOO/+0f72w7Zg=="
+<script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/gh/jerroydmoore/leaflet-button@master/L.Control.Button.js"></script>
 <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
-<script src='https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js'></script>
+<script src='https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster-src.js'></script>
 <script>
     window.filamentMaps = window.filamentMaps || [];
-
 
     L.Popup.prototype._animateZoom = function (e) {
         if (!this._map) {
@@ -16,6 +14,15 @@
             anchor = this._getAnchor();
         L.DomUtil.setPosition(this._container, pos.add(anchor));
     }
+
+    L.Marker.prototype._animateZoom = function (opt) {
+        if (!this._map) {
+            return
+        }
+
+        var pos = this._map._latLngToNewLayerPoint(this._latlng, opt.zoom, opt.center).round();
+        this._setPos(pos);
+    };
 
     L.Tooltip.prototype._updatePosition = function (e) {
         if (!this._map) {
